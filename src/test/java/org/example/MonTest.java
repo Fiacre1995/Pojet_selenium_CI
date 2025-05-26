e1ec179eea01b9f99d543d4fd8939282dbe13d3b
Fiacre1995 <a.fiacre.dion@gmail.com>
2025-05-26 19:58:40 +0200

    premier commit

---
+++ src/test/java/org/example/MonTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MonTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void testGoogle() {
+        driver.get("https://www.google.com");
+        Assertions.assertEquals("Google", driver.getTitle());
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
